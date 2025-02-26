7e6f5f1214cdf215614d40935def1c6d637bbd97
SHYE AVRAHAM <shye.avraham1@gmail.com>
2025-02-26 15:54:50 +0200

    check

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,12 +1,14 @@
 from src.calculator import add
 import pytest
+import allure
 
 
+@allure.tag("test add")
 def test_add():
     result = add(3, 4)
     assert result == 7
 
-
+@allure.tag("test add string")
 def test_add_string():
     with pytest.raises(TypeError):
         add("string", 4)
--- tests/test_division.py
+++ tests/test_division.py
@@ -6,12 +6,12 @@ def test_divide():
     result = divide(3, 2)
     assert result == 1.5
 
-
+@allure.tag("divide by zero")
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         divide(3, 0)
 
-
+@allure.tag("divide string")
 def test_divide_string():
     with pytest.raises(TypeError):
         divide("string", 2)
